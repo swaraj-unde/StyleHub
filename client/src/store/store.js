f66319aa5b705a65bebe7fadeca493189be851e6
Swaraj <swarajunde1@gmail.com>
2026-06-08 23:20:55 +0530

    Add adminProduct reducer to Redux store configuration

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice/index.js";
+import adminProductReducer from "./admin/products-slice/index.js";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    adminProduct: adminProductReducer,
   },
 });
 
