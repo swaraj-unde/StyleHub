efe799546c0685d991d1e5dd5aa867be11de3e6a
Swaraj <swarajunde1@gmail.com>
2026-06-08 23:21:12 +0530

    Implement CRUD operations for products in admin controller

--- server/src/controllers/admin/products.controller.js
+++ server/src/controllers/admin/products.controller.js
@@ -1,14 +1,12 @@
+import { Product } from "../../models/product.model.js";
 import { imageUploadUtil } from "../../utils/cloudinary.js";
 
 const handleImageUpload = async (req, res) => {
   try {
-
-
     const b64 = Buffer.from(req.file.buffer).toString("base64");
     const url = `data:${req.file.mimetype};base64,${b64}`;
 
     const result = await imageUploadUtil(url);
-
 
     return res.status(200).json({
       success: true,
@@ -24,4 +22,141 @@ const handleImageUpload = async (req, res) => {
   }
 };
 
-export default handleImageUpload;
+const addProduct = async (req, res) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+
+    const product = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    });
+
+    await product.save();
+    return res.status(200).json({
+      success: true,
+      message: "Product added Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error While Adding The Product",
+      error: error.message,
+    });
+  }
+};
+
+const fetchAllProducts = async (req, res) => {
+  try {
+    const listOfProducts = await Product.find({});
+
+    return res.status(200).json({
+      success: true,
+      message: "Products fetched Successfully",
+      data: listOfProducts,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error While fetching The Products",
+      error: error.message,
+    });
+  }
+};
+
+const editProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product Doesn't Exist",
+      });
+    }
+
+    product.image = image || product.image;
+    product.title = title || product.title;
+    product.description = description || product.description;
+    product.category = category || product.category;
+    product.brand = brand || product.brand;
+    product.price = price ?? product.price;
+    product.salePrice = salePrice ?? product.salePrice;
+    product.totalStock = totalStock ?? product.totalStock;
+
+    const updatedProduct = await product.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Product updated successfully",
+      data: updatedProduct,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error While Editing The Product",
+      error: error.message,
+    });
+  }
+};
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product Doesn't Exist",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+      data: product,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error While Deleting The Product",
+      error: error.message,
+    });
+  }
+};
+
+export {
+  handleImageUpload,
+  addProduct,
+  deleteProduct,
+  editProduct,
+  fetchAllProducts,
+};
