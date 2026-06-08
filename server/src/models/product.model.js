f41bc6690397c175d152a4267313c5a3798ca6c7
Swaraj <swarajunde1@gmail.com>
2026-06-08 23:20:15 +0530

    Product model

---
+++ server/src/models/product.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      default: "Any",
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    salePrice: {
+      type: Number,
+      default: 0,
+    },
+    totalStock: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Product = mongoose.model("Product", ProductSchema);
