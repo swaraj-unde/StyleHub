7e79c9c3d4873d38595daac13ff8c85b64744d6f
Swaraj <swarajunde1@gmail.com>
2026-06-08 19:46:34 +0530

    user models

---
+++ server/src/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = mongoose.model("User", UserSchema);
